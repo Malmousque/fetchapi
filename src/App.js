b6cab85fe4bafe0f89988820498fb55fa2877d9c
Malmousque <mathiastranzer@gmail.com>
2018-10-16 10:13:13 +0200

    page d'accueil corrigée

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpson : {},
+      simpson : {
+        image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+        character: "Nelson Muntz",
+        quote: "Shoplifting is a victimless crime, like punching someone in the dark."
+
+        
+      },
     }
 
   }
